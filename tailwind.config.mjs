d7793a24b58262241f6d45c00f649dff27a7c971
Luxe17 <hanson1717@live.com>
2023-11-09 15:16:19 -0800

    Project setup

---
+++ tailwind.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+import preline from 'preline/plugin.js'
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+}
