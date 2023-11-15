1067cf4151fa7a9dbd859ff066fa644aa4d85225
Luxe17 <hanson1717@live.com>
2023-11-15 09:40:22 -0800

    Changed tailwind config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,11 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+import preline from 'preline/plugin.js'
+
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: [
+		'./public/**/*.astro',        
+		'./src/**/*.{astro,js,jsx,ts,tsx,vue}',        
+		'node_modules/preline/dist/*.js',
+	],
 	theme: {
-		extend: {},
-	},
-	plugins: [require('daisyui')],
-	daisyui: {
-		themes: ["night", "winter"]
-	}
+		colors: {
+			'blue': '#5383E8',
+			'purple': '#7e5bef',
+			'pink': '#ff49db',
+			'orange': '#ff7849',
+			'green': '#13ce66',
+			'red': '#E84057',
+			'gray-dark': '#DBE0E4',
+			'gray': '#ebeef1',
+			'gray-light': '#F7F7F9',
+		},
+	  },
+	plugins: [preline],
 }
