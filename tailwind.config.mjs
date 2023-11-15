1ef697a73179b85c66861589ba9bd76fe6c4d256
Luxe17 <hanson1717@live.com>
2023-11-15 11:12:58 -0800

    Revert "Changed tailwind config"
    This reverts commit 1067cf4151fa7a9dbd859ff066fa644aa4d85225.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,24 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-import preline from 'preline/plugin.js'
-
 export default {
-	content: [
-		'./public/**/*.astro',        
-		'./src/**/*.{astro,js,jsx,ts,tsx,vue}',        
-		'node_modules/preline/dist/*.js',
-	],
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		colors: {
-			'blue': '#5383E8',
-			'purple': '#7e5bef',
-			'pink': '#ff49db',
-			'orange': '#ff7849',
-			'green': '#13ce66',
-			'red': '#E84057',
-			'gray-dark': '#DBE0E4',
-			'gray': '#ebeef1',
-			'gray-light': '#F7F7F9',
-		},
-	  },
-	plugins: [preline],
+		extend: {},
+	},
+	plugins: [require('daisyui')],
+	daisyui: {
+		themes: ["night", "winter"]
+	}
 }
